41df73494fdc82f3b873db5d7c1a0851d9113f3b
RenatBax <renchik_125.111@mail.ru>
2022-02-14 15:23:26 +0300

    Тренировка с библиотеками

---
+++ standartploot.py
@@ -0,0 +1,6 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.linspace(0,10,50) # Создайте список равномерно распределенных чисел по диапазону
+# plt.plot(x, np.sin(x)) # Постройте синус каждой точки x
+# plt.show() # Отображение графика
